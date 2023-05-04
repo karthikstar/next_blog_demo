cccd54fbbbea9268e1ef005bb00a15292a4a866c
karthikstar <64301417+karthikstar@users.noreply.github.com>
2023-05-04 16:07:28 +0800

    dynamic routes done

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
+import Link from 'next/link'; // to add links to each post page, use link component
+import Date from '../components/date';
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -30,11 +33,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,11 +1,16 @@
 import Layout from '../../components/layout';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+
+import Head from 'next/head';
+import Date from '../../components/date';
+
 
 // Implementing getStaticProps
 // The post page is now using the getPostData function in getStaticProps to get the post data and return it as props.
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id); // getPostData() is a async function
   return {
     props: {
       postData,
@@ -15,6 +20,7 @@ export async function getStaticProps({ params }) {
 
 
 //Finally, we'll import the getAllPostIds function and use it inside getStaticPaths. 
+// returns an array of possible values for id
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
@@ -26,11 +32,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
       <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
       </Layout>
     );
   }
